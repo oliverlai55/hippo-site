9fffd3027cfb76909058a269079e3c890c4c199f
Wei lun Lai <oliverlai55@gmail.com>
2016-01-14 15:18:21 -0500

    adding submit_post.php

--- submit_post.php
+++ submit_post.php
@@ -1,6 +1,19 @@
 <?php
 
-include('inc/db_connect.php');
+	include('inc/db_connect.php');
+	try{
+		DB::insert('posts', array(
+			'id' => '',
+			'user_id' => $_SESSION['uid'],
+			'body' => $_POST['newPost'],
+			'timestamp' => time()
 
-$
+		));
+	}catch(MeekroDBException $e){
+		header('Location: /submit.php?error=yes')
+		exit;
+	}
+
+	$_SESSION['username'] = $username;
+	$_SESSION['uid'] = DB::insertId();
 ?>
