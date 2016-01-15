a7b70cdb37626e3bee729aaa9a5684b6142abb27
Wei lun Lai <oliverlai55@gmail.com>
2016-01-15 13:28:19 -0500

    added follow.php page

---
+++ follow.php
@@ -0,0 +1,33 @@
+<?php include 'inc/db_connect.php';?>
+
+<?php
+	
+	$results = DB::query(
+		"SELECT users.id, users.username,");
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php include('inc/head.php') ?>
+</head>
+<body>
+	<?php include('inc/header.php') ?>
+
+	<div class="container">
+		<div class="row">
+			<?php
+				foreach($result as $user){
+					print '<div class="row">';
+						print '<div class="user">'.$user['username'].'</div>';
+						print '<div class="follow-user"><button type="button class="btn btn-primary" uid='.$user['id'].'>;
+					print '</div>';
+				}
+			?>
+		</div>
+	</div>
+</body>
+</html>
