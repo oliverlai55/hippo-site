19825aa404e131b20fc0cba5c00ed644c6273314
Wei lun Lai <oliverlai55@gmail.com>
2016-01-15 10:42:20 -0500

    added more changes to index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if($_GET['logout']){
 	header('Location: index.php');
 }
 
-if($_SESSION)['username']{
+if($_SESSION['username']){
 	$results = DB::query("SELECT * FROM posts ORDER BY timestamp desc limit 30");
 }
 
@@ -18,14 +18,10 @@ if($_SESSION)['username']{
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha256-Av2lUNJFz7FqxLquvIFyxyCQA/VHUFna3onVy+5jUBM= sha512-zyqATyziDoUzMiMR8EAD3c5Ye55I2V3K7GcmJDHbL49cXzFQCEA6flSKqxEzwxXqq73/M4A0jKFFJ/ysEhbExQ==" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
-	<script src="script.js"></script>
-
+	<title>Save the Hippos</title>
+<?php
+	include 'inc/head.php';
+?>
 </head>
 <body>
 	<div class="container">
@@ -35,45 +31,17 @@ if($_SESSION)['username']{
 				<h1>The Hippo Guardian</h1>
 			</div>
 		</div>
-		<div class="row">
-			<div id="header" class="col-sm-12">
-				<h2>Join the millions fighting for Hippos today!</h2>
-			</div>
-		</div>
-		<form class="form-horizontal">
-			<div class="control-group">
-				<label class="control-label" for="inputEmail">Full Name</label>
-				<div class="controls">
-					<input type="text" id="inputEmail" placeholder="Full Name">
-				</div>
-			</div>
-			<div class="control-group">
-				<label class="control-label" for="inputEmail">Email</label>
-				<div class="controls">
-					<input type="text" id="inputEmail" placeholder="Email">
-				</div>
-			</div>
-			<div class="control-group">
-				<label class="control-label" for="inputEmail">Username</label>
-				<div class="controls">
-					<input type="text" id="inputEmail" placeholder="Username">
-				</div>
-			</div>
-			<div class="control-group">
-				<label class="control-label" for="inputPassword">Password</label>
-				<div class="controls">
-					<input type="password" id="inputPassword" placeholder="Password">
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="controls">
-					<label class="checkbox">
-						<input type="checkbox"> Remember me
-					</label>
-					<button type="submit" class="btn">Sign in</button>
-				</div>
-			</div>
-		</form>
+		<?php
+			if(isset($_SESSION['username'])){
+				foreach($results as $result){
+					$user = DB::query("SELECT * FROM users WHERE uid=%i",$result['uid']);
+					print "<div class='post-container'><div class='user'><a href='/user.php?user=". $result['uid'] ."'>@" . $user[0]['username'] . "</a></div>";
+					print "<div class='post-content'>" . $result['body'] . "</div>";
+					print "div class='post-time'>". $result['timestamp'] . "</div></div>";
+				}
+			}
+		?>
+		
 		<?php include('inc/footer.php');?>
 	</div>
 </body>
