c5457f7163cb8a289176a4b474695da39fbd484f
Wei lun Lai <oliverlai55@gmail.com>
2016-01-18 14:47:49 -0500

    changed follow.php

--- follow.php
+++ follow.php
@@ -18,16 +18,26 @@
 	<?php include('inc/header.php') ?>
 
 	<div class="container">
-		<div class="row">
-			<?php
-				foreach($result as $user){
-					print '<div class="row">';
-						print '<div class="user">'.$user['username'].'</div>';
-						print '<div class="follow-user"><button type="button" class="btn btn-primary" uid='.$user['id'].'>;
-					print '</div>';
-				}
-			?>
+		<?php foreach($results as $user){
+
+	    	if(in_array($user['id'],$following_array)){
+	    		$button_text = 'Unfollow';
+	    		$follow = "unfollow";
+	    		$button_class = 'btn-default';
+	    	}else{
+	    		$button_text = 'Follow';
+	    		$follow = "followed";
+	    		$button_class = 'btn-primary';
+
+	    	}
+	    	print '<div class="user">';
+	    		// print '<div class="user-name col-md-6 text-left">'.$user['realname'].'</div>';
+	    		print '<div class="user-hippo col-md-12 text-left">'.$user['username'].'</div>';
+	    		print '<button type="button" class="btn '.$button_class.' col-md-2 text-left follow-button" data-follow="'.$follow.'" uid='.$user['id'].'>'.$button_text.'</button>';
+	    	print '</div>';
+	    }
+	    ?>
+
 		</div>
-	</div>
 </body>
 </html>
