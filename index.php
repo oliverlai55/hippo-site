d6da59c82d9daab3fa779b2376d5d0a4f577c6d8
Wei lun Lai <oliverlai55@gmail.com>
2016-01-20 16:11:09 -0500

    finished the index.php updates

--- index.php
+++ index.php
@@ -12,16 +12,16 @@ if(!$_SESSION['username']){
 	header('Location: login.php');
 }
 	
-	$results_following = DB::query("SELECT distinct(uid_to_follow) FROM following following
-		WHERE following.uid=%i" , $_SESSION['uid'];
+	$results_following = DB::query("SELECT distinct(user_id_to_follow) FROM following following
+		WHERE following.user_id=%i" , $_SESSION['user_id']);
 		$last = count($results_following);
 		if($last > 0){
 			$i = 0;
 			$following_array = '';
 			foreach($results_following as $following){
 				$i++;
-				$following_array .= $following['uid_to_follow'];
-				if($i != $last){$following_array .=",";}
+				$following_array .= $following['user_id_to_follow'];
+				if($i != $last){$following_array .= ",";}
 			}
 			$posts = DB::query("SELECT posts.body, posts.timestamp, users.username, users.uid, posts.pid FROM posts
 				LEFT JOIN users on posts.uid = users.uid
@@ -29,12 +29,10 @@ if(!$_SESSION['username']){
 				ORDER BY posts.timestamp desc");
 		}else{
 			$posts = DB::query(
-				"SELECT posts.body, posts.timestamp, users.username FROM posts
+				"SELECT posts.content, posts.timestamp, users.username FROM posts
 					LEFT JOIN users on posts.uid=users.uid
 					ORDER BY posts.timestamp desc limit 30");
 		}
-
-
 
 ?>
 
@@ -66,14 +64,16 @@ if(!$_SESSION['username']){
 				}
 
 				
-				print "<div class='post-container'><div class='user'><a href='/user.php?user=". $result['uid'] ."'>@" . $user[0]['userName'] . "</a></div>";
-				print "<div class='post-content'>" . $result['content'] . "</div>";
-				print "<div class='post-time'>". $result['timestamp'] ."</div>";
-				print "<div class='vote-container'><a href='process_vote.php?pid=".$result['id']."&uid=".$result['uid']."&vote=up'><span class='glyphicon glyphicon-menu-up up-vote'></span></a>";
-				print "<span class='votes'>".$result['pid']."</span>";
-				print "<a href='process_vote.php?pid=".$result['id']."&uid=".$result['uid']."&vote=down'><span class='glyphicon glyphicon-menu-down down-vote'></span></a></div>";
+				print "<div class='post-container'><div class='user'><a href='/user.php?user=". $post['uid'] ."'>@" . $post['userName'] . "</a></div>";
+				print "<div class='post-content'>" . $post['content'] . "</div>";
+				print "<div class='post-time'>". $post['timestamp'] ."</div>";
+				print "<div class='vote-container'><a href='process_vote.php?pid=".$post['pid']."&uid=".$post['uid']."&vote=up'><span class='glyphicon glyphicon-menu-up up-vote'></span></a>";
+				print "<span class='votes'>".$post_votes ."</span>";
+				print "<a href='process_vote.php?pid=".$post['id']."&uid=".$post['uid']."&vote=down'><span class='glyphicon glyphicon-menu-down down-vote'></span></a></div>";
 				print "</div>";
 			}
+		}else{
+			print '<div class="start-following"> Follow some people <a href="/follow.php">here</a> to see posts</div>';
 		}
 		?>
 		
--- process_vote.php
+++ process_vote.php
@@ -7,6 +7,12 @@
 		$vote = -1;
 	}
 
+	$result = DB::query("SELECT * FROM post_votes WHERE uid='".$_GET['uid']."' AND pid = '") $_GET['pid'] . "'");
+
+	if((sizeof($result) > 0) && ($result[0]['vote'] == 1) && ($vote == -1)){
+		DB::update('post_votes',array('vote' => $vote),"pid=%i", $_GET['pid']);
+	}
+
 	DB::insert('post_votes', array(
 		'pid' => $_GET['pid'],
 		'uid' => $_GET['uid'],
