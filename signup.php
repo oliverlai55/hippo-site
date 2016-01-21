dc06eef4d414fed42aae9f7132f8d238becac554
Wei lun Lai <oliverlai55@gmail.com>
2016-01-21 01:16:54 -0500

    changed signup.php

--- signup.php
+++ signup.php
@@ -7,29 +7,28 @@ if($_SESSION['username']){
 
 if(isset($_POST['userName'])){
 	if($_POST['password'] !== $_POST['password-confirm']){
-		header('Location: /signup.php?password=fail');
-	}
-
-	$fullName = $_POST['name'];
-	$email = $_POST['email'];
-	$username = $_POST['userName'];
-	$hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		$fullName = $_POST['name'];
+		$email = $_POST['email'];
+		$username = $_POST['userName'];
+		$hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 		//User submitted something
 		//check to see if this user is in the DB
-	$result = DB::query("SELECT * FROM users WHERE username = '" . $_POST['userName']."' OR email = '" . $email . "'");
-
-	if(!$result){
-		$result = DB::query("INSERT INTO users (name,email,username,password) VALUES
+		$result = DB::query("SELECT * FROM users WHERE username = '" . $_POST['userName']."' OR email = '" . $email . "'");
+		if(!$result){
+			$result = DB::query("INSERT INTO users (name,email,username,password) VALUES
 			('" . $fullName . "','" . $email . "','" . $username . "','" . $hashed_password . "')" );
-		$_SESSION['username'] = $username;
-		$_SESSION['uid'] = DB::insertId();
-		header('Location: /index.php');
+			$_SESSION['username'] = $username;
+			$_SESSION['uid'] = DB::insertId();
+			header('Location: /index.php');
+		}else{
+			header('Location: /signup.php?register=failure');
+		}
 	}else{
-		header('Location: /signup.php?register=failure');
+		header('Location: /signup.php?password=fail');
 	}
 
-}
+
 ?>
 
 <!DOCTYPE html>
